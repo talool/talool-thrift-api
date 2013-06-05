5c3c206adab8e488903cb9cbae817e0aec9722d8
Chris Lintz <christopher.justin@gmail.com>
2013-06-04 21:42:47 -0600

    cocoa Core.thrift generation

--- src/main/thrift/compile-thrift.sh
+++ src/main/thrift/compile-thrift.sh
@@ -13,7 +13,11 @@ thrift -v -o $scriptDir -I $includeDir --gen java:java5  src/main/thrift/Core.th
 
 thrift -v -o $scriptDir -I $includeDir --gen java:java5  src/main/thrift/CustomerService.thrift 
 
+thrift -v -o $scriptDir -I $includeDir --gen cocoa src/main/thrift/Core.thrift
+
 thrift -v -o $scriptDir -I $includeDir --gen cocoa src/main/thrift/CustomerService.thrift
+
+
 
 echo "*** Moving $scriptDir/gen-java/* to src/main/java/. ***"
 cp -r  $scriptDir/gen-java/* src/main/java/.
