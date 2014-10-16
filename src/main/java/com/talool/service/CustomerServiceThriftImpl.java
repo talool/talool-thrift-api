462fbb86b0e121e83a4053b2ac577384082910f8
Doug McCuen <doug@talool.com>
2014-10-16 10:31:30 -0600

    pass different exp date for iphone vs android bc the logic on the phone is different

--- src/main/java/com/talool/service/CustomerServiceThriftImpl.java
+++ src/main/java/com/talool/service/CustomerServiceThriftImpl.java
@@ -436,11 +436,23 @@ public class CustomerServiceThriftImpl implements CustomerService_t.Iface
 		{
 			setThreadLocalServiceHeaders(customerService);
 			
-			Calendar c = Calendar.getInstance();
-			c.roll(Calendar.DAY_OF_YEAR, -31);
-			Date expiresAfter = c.getTime();
 			
-			dealAcquires = customerService.getDealAcquires(UUID.fromString(token.getAccountId()), UUID.fromString(merchantId), null, expiresAfter);
+			
+			final HttpServletRequest request = RequestUtils.getRequest();
+			final String userAgent = request.getHeader(Constants.HEADER_USER_AGENT);
+			Calendar c = Calendar.getInstance();
+			if (StringUtils.containsIgnoreCase(userAgent, "iphone"))
+			{
+				// Pass an old date for iphone, bc the data is managed on the device
+				c.roll(Calendar.YEAR, -2);
+			}
+			else
+			{
+				// Pass the date for Android, bc any filtering of expired offers needs to happen here
+				c.roll(Calendar.DAY_OF_YEAR, -31);
+			}
+			
+			dealAcquires = customerService.getDealAcquires(UUID.fromString(token.getAccountId()), UUID.fromString(merchantId), null, c.getTime());
 
 			return CollectionUtils.isEmpty(dealAcquires) ? EMPTY_ACQUIRES : ConversionUtil.convertToThriftDealAcquires(dealAcquires);
 
